a922b27859619213526cfbe896b58285c281e64a
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-02-20 19:52:12 -0600

    refactor: clean up

--- app/poro/combined_item_shop.rb
+++ app/poro/combined_item_shop.rb
@@ -1,16 +1,11 @@
 class CombinedItemShop
-  attr_reader :final_price, :name, :bundle_image, :item_images, :type, :id
+  attr_reader :final_price, :name, :image, :type, :id
 
   def initialize(item)
     @final_price = item[:finalPrice]
     @name = item[:items][0][:name]
     @type = item[:items][0][:type]
-    if item[:bundle]
-      @bundle_image = item[:bundle][:image]
-      @item_images = [item[:items][0][:images][:icon]]
-    else
-      @item_images = [item[:items][0][:images][:icon]]
-    end
+    @image = item[:bundle] ? item[:bundle][:image] : item[:items][0][:images][:icon]
     @id = item[:items][0][:id]
   end
 end
--- app/serializers/combined_item_shop_serializer.rb
+++ app/serializers/combined_item_shop_serializer.rb
@@ -1,4 +1,4 @@
 class CombinedItemShopSerializer
   include JSONAPI::Serializer
-  attributes :final_price, :name, :bundle_image, :item_images, :type
+  attributes :final_price, :name, :image, :type
 end
