621b014ebb82df44191675d7847ad2eaacaf474f
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-02-27 23:47:51 -0600

    chore: clean up

--- spec/request/api/v1/item_shop_combined_spec.rb
+++ spec/request/api/v1/item_shop_combined_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Combined Item Shop', type: :request do
       get '/api/v1/item_shop'
 
       result = JSON.parse(response.body, symbolize_name: true)
-    # require 'pry'; binding.pry
+
       expect(response).to be_successful
       expect(response.status).to eq(200)
       expect(result).to be_a(Hash)
