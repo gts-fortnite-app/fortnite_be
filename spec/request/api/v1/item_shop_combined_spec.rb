946677b49502e8a4c4d8a2dc30c8d48e2dc0fb8f
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-03-23 13:16:56 -0500

    spec: update itemshop spec using vcr and webmock

--- spec/request/api/v1/item_shop_combined_spec.rb
+++ spec/request/api/v1/item_shop_combined_spec.rb
@@ -1,23 +1,32 @@
 require 'rails_helper'
 RSpec.describe 'Combined Item Shop', type: :request do
-  context 'GET /item-shop' do
-    scenario 'returns fortnite daily item shop info' do
+  context '#index' do
+    scenario 'returns fortnite daily item shop info', :vcr do
       get '/api/v1/item_shop'
+      expect(response).to be_successful
 
       result = JSON.parse(response.body, symbolize_name: true)
 
-      expect(response).to be_successful
       expect(response.status).to eq(200)
       expect(result).to be_a(Hash)
       expect(result).to have_key('data')
 
-      result['data'].each do |item|
-        expect(item['attributes']).to have_key('final_price')
-        expect(item['attributes']).to have_key('name')
+      items = result['data']
+
+      items.each do |item|
+        attributes = item['attributes']
+        expect(attributes['final_price']).to be_a(Integer)
+        expect(attributes).to have_key('final_price')
+        expect(attributes['name']).to be_a(String)
+        expect(attributes).to have_key('name')
+        expect(attributes['image']).to be_a(String)
         expect(item['attributes']).to have_key('image')
-        expect(item['attributes']).to have_key('type')
-        expect(item['attributes']).to have_key('date')
-        expect(item['attributes']).to have_key('vbuck_icon')
+        expect(attributes['type']).to be_a(Hash)
+        expect(attributes).to have_key('type')
+        expect(attributes['date']).to be_a(String)
+        expect(attributes).to have_key('date')
+        expect(attributes['vbuck_icon']).to be_a(String)
+        expect(attributes).to have_key('vbuck_icon')
       end
     end
   end
