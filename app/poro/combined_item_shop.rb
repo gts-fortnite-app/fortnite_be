6d0a99fb9c95725d518b61607822b1eb835e7bf2
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-03-23 23:40:09 -0500

    remove: type from api

--- app/poro/combined_item_shop.rb
+++ app/poro/combined_item_shop.rb
@@ -1,12 +1,12 @@
 class CombinedItemShop
-  attr_reader :final_price, :name, :image, :type, :id, :date, :vbuck_icon
+  attr_reader :final_price, :name, :image, :id, :date, :vbuck_icon
 
   def initialize(item)
     @date = item[:date]
     @vbuck_icon = item[:vbuckIcon]
     @final_price = item[:finalPrice]
     @name = item[:items][0][:name]
-    @type = item[:items][0][:type]
+    # @type = item[:items][0][:type]
     @image = item[:bundle] ? item[:bundle][:image] : item[:items][0][:images][:icon] || item[:items][0][:images][:featured]
     @id = item[:items][0][:id]
   end
--- spec/facades/item_shop_facade_spec.rb
+++ spec/facades/item_shop_facade_spec.rb
@@ -8,14 +8,14 @@ RSpec.describe "Item Shop Facade" do
       items.each do |item|
         expect(item).to respond_to(:final_price).or respond_to(:name)
           .or respond_to(:image)
-          .or respond_to(:type)
+          # .or respond_to(:type)
           .or respond_to(:date)
           .or respond_to(:vbuck_icon)
 
         expect(item.final_price).to be_a(Integer)
         expect(item.name).to be_a(String)
         expect(item.image).to be_a(String)
-        expect(item.type).to be_a(Hash)
+        # expect(item.type).to be_a(Hash)
         expect(item.date).to be_a(String)
         expect(item.vbuck_icon).to be_a(String)
       end
