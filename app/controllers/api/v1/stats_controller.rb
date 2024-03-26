f28af2ab83917001385dd67544596d92ef591b62
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-03-25 23:21:27 -0500

    request: user cant search a name without the name

--- app/controllers/api/v1/stats_controller.rb
+++ app/controllers/api/v1/stats_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::StatsController < ApplicationController
   
-  def index
-    render json: StatsSerializer.new(StatsFacade.player_stats(params[:name]))
+  def show
+    if params[:name].blank?
+      raise ArgumentError, 'missing name parameter'
+    else
+      render json: StatsSerializer.new(StatsFacade.player_stats(params[:name])), status: :ok
+    end
+  rescue ArgumentError => e
+    render json: { status: 400, error: e.message }, status: :bad_request
+  rescue StandardError => e
+    render json: { status: 500, error: e.message }, status: :internal_server_error
   end
 end
