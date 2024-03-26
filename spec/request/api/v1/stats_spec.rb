68275b3ff1b1f4fab4c26bcde6ae2b83174f78f9
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-03-25 23:21:07 -0500

    add: error handling for a blank name

--- spec/request/api/v1/stats_spec.rb
+++ spec/request/api/v1/stats_spec.rb
@@ -16,5 +16,22 @@ RSpec.describe 'player stats search', type: :request do
       expect(response.status).to eq(200)
       expect(response.body).to be_a(String)
     end
+
+    scenario 'sad path, returns code 404' do 
+      name = ''
+
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+
+      get '/api/v1/stats', headers: headers, params: name
+
+      error_response = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to_not be_successful
+      expect(response.content_type).to match(a_string_including("application/json"))
+      expect(response.status).to eq(400)
+      expect(response).to have_http_status(:bad_request)
+      expect(error_response).to be_a(Hash)
+      expect(error_response).to have_key(:error)
+      expect(error_response[:status]).to eq(400)
+    end
   end
 end
