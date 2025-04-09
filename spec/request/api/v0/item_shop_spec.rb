f1a18acf945db37999f179e3a5ab271a0ce9a4c6
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2025-04-08 20:51:38 -0500

    update test based off new PORO(CombinedItemShop) structure.
    Skip a V0 test. Might keep the file layout for something out or just delete later

--- spec/request/api/v0/item_shop_spec.rb
+++ spec/request/api/v0/item_shop_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe 'Item Shop', type: :request do 
   context "GET /item-shop" do 
-    scenario "returns fortnite daily item shop info" do
+    xscenario "returns fortnite daily item shop info" do
         get '/api/v0/item_shop'
 
         result = JSON.parse(response.body, symbolize_name: true)
